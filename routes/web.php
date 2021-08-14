70a088e3092b5694159b14434eded80bd55625c1
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2021-08-14 08:53:26 -0700

    json file for OIAA

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('oiaa', function () {
+
+    //fetch data
+    $rows = Http::get(
+        'https://sheets.googleapis.com/v4/spreadsheets/' .
+            env('GOOGLE_SHEET_ID') . '/values/A1:Z10000?key=' .
+            env('GOOGLE_SHEET_API_KEY')
+    )['values'];
+
+    //get columns
+    $columns = array_map(function ($column) {
+        return Str::slug($column, '_');
+    }, array_shift($rows));
+    $column_count = count($columns);
+
+    //loop through and format rows
+    $rows = array_map(function ($row) use ($columns, $column_count) {
+        extract(array_combine($columns, array_pad($row, $column_count, null)));
+        return compact('name', 'times', 'timezone', 'url', 'phone', 'access_code', 'email', 'types', 'formats', 'notes');
+    }, $rows);
+
+    Storage::put('oiaa.json', json_encode($rows), 'public');
+
+    return 'done!';
 });
