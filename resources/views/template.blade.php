c64a48a02e35a0e336057475c4499e75d6b8e8a0
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2021-08-16 21:41:40 -0700

    adding aasanjose

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Meeting Guide Sheets Import</title>
     <link rel="stylesheet" href="/css/app.css" />
+    <!-- <script src="/js/app.js" defer></script> -->
+    <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 
 <body>
