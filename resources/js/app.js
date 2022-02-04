867961ca5305e29bd995b5080167ad581d9133b7
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2022-02-03 20:48:22 -0800

    laravel upgrade, new vision

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,1 @@
-var dd = document.getElementById("dropdown-desktop");
-
-dd.getElementsByTagName("button")[0].addEventListener("click", function () {
-    dd.getElementsByTagName("div")[0].classList.toggle("hidden");
-});
-
-document
-    .getElementById("dropdown-mobile-button")
-    .addEventListener("click", function () {
-        document.getElementById("dropdown-mobile").classList.toggle("hidden");
-    });
+require('./bootstrap');
