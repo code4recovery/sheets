1cd3dfee3ff233f410e4c3940ac8bf293441cb03
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2023-03-10 21:50:26 -0800

    adding dark mode

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,12 +4,23 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script>
+        function setColorMode(dark) {
+            document.documentElement.setAttribute('data-bs-theme', dark ? 'dark' : 'light');
+        }
+        setColorMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
+        window
+            .matchMedia("(prefers-color-scheme: dark)")
+            .addEventListener("change", function(e) {
+                setColorMode(e.matches);
+            });
+    </script>
     <title>{{ env('APP_NAME') }}</title>
 </head>
 
-<body class="bg-light">
+<body>
     <main class="container my-5">
         <div class="row">
             <div class="col-md-6 offset-md-3">
