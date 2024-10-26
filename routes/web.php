10e3650351f857609e3058eb9383e02cab000b0b
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2024-10-26 08:51:48 -0700

    "TSML" mode, syntax updates, remove obsolete route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ Route::view('/', 'welcome');
 
 Route::post('/', function () {
     $parts = explode('/', request('sheetUrl'));
-    if (count($parts) !== 7) return redirect()->back();
+    if (count($parts) !== 7) {
+        return redirect()->back();
+    }
     return redirect('/' . $parts[5]);
 });
 
@@ -17,17 +19,11 @@ Route::get('oiaa', [OiaaController::class, 'oiaa']);
 
 Route::get('aasfmarin', [AirtableController::class, 'aasfmarin']);
 
-Route::get('puget-sound', function () {
-    $redirectTo = request('redirectTo');
-    list($d54, $d54warnings) = Controller::fetch('15ABWfNki5wWyufgoC3V3EZZ_DRGmAbz3SecoFBND1yY');
-    list($pscso, $pscsowarnings) = Controller::fetch('13W4lBuRWKpnHNOC_3wTXI5anLVOkyvMmyn4Wvqx1z3c');
-    $rows = array_merge($d54, $pscso);
-    $warnings = array_merge($d54warnings, $pscsowarnings);
-    $response = Controller::generate($rows, $warnings, 'puget-sound.json');
-    if (!empty($response['error'])) {
-        return redirect()->back()->with('error', $response['error'])->with('sheetUrl', 'https://docs.google.com/spreadsheets/d/' . $sheetId . '/edit#gid=0');
-    }
-    return ($redirectTo && !count($response['warnings'])) ? redirect($redirectTo) : view('done', $response);
+// "TSML" mode live-fetches and includes warnings
+Route::get('tsml/{sheetId}', function ($sheetId) {
+    $response = Controller::fetch($sheetId);
+    [$rows, $warnings] = $response;
+    return ['meetings' => $rows, 'warnings' => $warnings];
 });
 
 Route::get('{sheetId}', function ($sheetId, $redirectTo = false) {
@@ -36,7 +32,7 @@ Route::get('{sheetId}', function ($sheetId, $redirectTo = false) {
     if (!empty($response['error'])) {
         return redirect()->back()->with('error', $response['error'])->with('sheetUrl', 'https://docs.google.com/spreadsheets/d/' . $sheetId . '/edit#gid=0');
     }
-    list ($rows, $warnings) = $response;
+    [$rows, $warnings] = $response;
     $response = Controller::generate($rows, $warnings, $sheetId . '.json');
     if (!empty($response['error'])) {
         return redirect()->back()->with('error', $response['error'])->with('sheetUrl', 'https://docs.google.com/spreadsheets/d/' . $sheetId . '/edit#gid=0');
